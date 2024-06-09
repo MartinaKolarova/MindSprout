a55437643489cfdb6155e1e40a121a0cb355acf1
Martina Kolářová <ruzickova1.martina@gmail.com>
2024-06-09 18:14:20 +0200

    Share on last page

--- src/components/Tree/Tree.jsx
+++ src/components/Tree/Tree.jsx
@@ -2,12 +2,18 @@ import { Bubble } from './Bubble/Bubble';
 import { Dialog } from './Dialog/Dialog';
 import styles from './Tree.module.css';
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import {
+  FacebookMessengerShareButton,
+  FacebookMessengerIcon,
+  FacebookShareButton,
+  FacebookIcon,
+} from 'next-share';
 
 export const Tree = ({ category }) => {
   const [phase, setPhase] = useState('stabilization');
   const [selectedExercise, setSelectedExercise] = useState(null);
   const [exercises, setExercises] = useState([]);
-
   const currentPhaseExercises = exercises.filter((exercise) => {
     return exercise.targetId === phase;
   });
@@ -59,12 +65,35 @@ export const Tree = ({ category }) => {
       {phase === 'userHasFinished' ? (
         <>
           <div>
-            <p>Ahoj</p>
-            <button></button>
+            <p>
+              Máme radost pokud ti naše stránka pomohla cítit se lépe. Pokud bys
+              ji rád/a doporučil někomu jinému, můžeš nás sdílet.
+            </p>
+            <FacebookShareButton
+              url={'https://github.com/MartinaKolarova/MindSprout'}
+              quote={
+                'MindSprout je stránka pomáhající teenagerům pečovat o své psychické zdraví'
+              }
+              hashtag={'#nextshare'}
+            >
+              <FacebookIcon size={32} round />
+            </FacebookShareButton>
+
+            <FacebookMessengerShareButton
+              url={'https://github.com/MartinaKolarova/MindSprout'}
+              appId={''}
+            >
+              <FacebookMessengerIcon size={32} round />
+            </FacebookMessengerShareButton>
           </div>
           <div>
-            <p></p>
-            <button></button>
+            <p>
+              Pokud se stále cítíš mizerně a tvůj problém je větší než naše
+              stránka dokáže obsáhnout. Můžeš využít sekci kontakty
+            </p>
+            <Link href="/kontakty" className={styles.buttonContact}>
+              Kontakty
+            </Link>
           </div>
         </>
       ) : (
