ef05b6342f0d3946500208b80be735b1d414b0ab
Martina Kolářová <ruzickova1.martina@gmail.com>
2024-06-09 14:39:08 +0200

    Contact styles in CSS

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,38 +2,59 @@ import styles from './Contact.module.css';
 
 export const Contact = () => {
   return (
-    <>
+    <div className={styles.contactMain}>
       <div className={`${styles.contact} ${styles.linkaBezpeci}`}>
-        <a href="https://www.linkabezpeci.cz/">Linka Bezpečí</a>
-        <p>116 111</p>
+        <a href="https://www.linkabezpeci.cz/">Linka Bezpečí: </a>
+        <br />
+        <a className={styles.phone} href="tel:116 111">
+          116 111
+        </a>
       </div>
+
       <div className={`${styles.contact} ${styles.locika}`}>
         <a href="https://www.detstvibeznasili.cz/jsem-dite-nebo-teenager">
           Linka centra Locika pro děti ohrožené domácím násilím:
         </a>
-        <p>734 441 233</p>
+        <br />
+        <a className={styles.phone} href="tel:734 441 233">
+          734 441 233
+        </a>
       </div>
       <div className={`${styles.contact} ${styles.krizoveCentrum}`}>
         <a href="https://www.ditekrize.cz/">
+          <br />
           Linka důvěry Dětského krizového centra:
-          <p>777 715 215, 241 484 149</p>
+        </a>
+        <br />
+        <a className={styles.phone} href="tel:777 715 215">
+          777 715 215
         </a>
       </div>
       <div className={`${styles.contact} ${styles.profem}`}>
         <a href="https://www.profem.cz">
-          Linka pro oběti domácího a sexuálního násilí PROFEM
-          <p>608 222 277</p>
+          <br />
+          Linka pro oběti domácího a sexuálního násilí PROFEM:
+        </a>
+        <br />
+        <a className={styles.phone} href="tel:608 222 277">
+          608 222 277
         </a>
       </div>
+
       <div className={`${styles.contact} ${styles.spondea}`}>
         <a href="https://www.spondea.cz/cz/kontakt">
           Centrum pro zneužívané a týrané děti Spondea:
         </a>
-        <p>541 235 511, 608 118 088</p>
+        <br />
+        <a className={styles.phone} href="tel:608 118 088">
+          608 118 088
+        </a>
       </div>
-      <a href="https://linkaztracenedite.cz/chat/">Linka Ztracené dítě</a>
-      <p>chat</p>
-      <div className={`${styles.contact} ${styles.ztraceneDite}`}></div>
-    </>
+      <div className={`${styles.contact} ${styles.ztraceneDite}`}>
+        <a href="https://linkaztracenedite.cz/chat/">
+          Linka Ztracené dítě (chat)
+        </a>
+      </div>
+    </div>
   );
 };
