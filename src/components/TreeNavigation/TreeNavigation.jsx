c09117b243b4d4af659395234331727d02bb8395
Martina Kolářová <ruzickova1.martina@gmail.com>
2024-06-15 22:06:30 +0200

    Positioning and effects on textContainer

--- src/components/TreeNavigation/TreeNavigation.jsx
+++ src/components/TreeNavigation/TreeNavigation.jsx
@@ -6,25 +6,28 @@ export const TreeNavigation = ({ onClick }) => {
   return (
     <div className={styles.navContainer}>
       <div className={styles.textContainer}>
-        <p className={styles.introduction}>
-          Tahle appka ti pomůže cítit se líp a naučit se řešit svoje problémy
-          tak, aby tě příště nerozhodily
-        </p>
-        <p className={styles.guidance}>
-          Vyber si oblast, kterou řešíš a projdi si tři kroky, díky
-          kterým se nebudeš cítit líp jen ty, ale i strom, který nutně
-          potřebuje pomoc.
-        </p>
+        <div className={styles.introduction}>
+          <p className={styles.textFocusIn}>
+            Tahle appka ti pomůže cítit se líp a naučit se řešit svoje problémy
+            tak, aby tě příště nerozhodily
+          </p>
+        </div>
+        <div className={styles.guidance}>
+          <p className={styles.textFocusIn}>
+            Vyber si oblast, kterou řešíš a projdi si tři kroky, díky kterým se
+            nebudeš cítit líp jen ty, ale i strom, který nutně potřebuje pomoc.
+          </p>
+        </div>
       </div>
       <div className={styles.buttonsContainer}>
         <button className={styles.navButton} onClick={() => onClick(1)}>
-          Problém s vrstevníky
+          Vztahy s vrstevníky
         </button>
         <button className={styles.navButton} onClick={() => onClick(2)}>
-          Problém s rodinou
+          Vztahy v rodině
         </button>
         <button className={styles.navButton} onClick={() => onClick(3)}>
-          Problém ve škole
+          Škola
         </button>
       </div>
     </div>
